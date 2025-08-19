b6de595e34cda0470f1664577f902d0aefa77d0b
ranjanmannige <ranjanmannige@gmail.com>
2025-08-19 16:08:04 -0400

    Added definition to prevent errors associated with Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import path from 'path'; // Import path module
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  define: {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false', // or 'true' if you need detailed info
+  },
   resolve: {
     alias: 
       {
